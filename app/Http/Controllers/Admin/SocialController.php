ad3e862589c55a86d7b0f413042055268a5a0e30
David Adeniran <david.adeniran2016@gmail.com>
2024-02-13 03:15:24 -0700

    2st Fix for google Oauth

--- app/Http/Controllers/Admin/SocialController.php
+++ app/Http/Controllers/Admin/SocialController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
-use Illuminate\Support\Facades\Auths;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class SocialController extends Controller
